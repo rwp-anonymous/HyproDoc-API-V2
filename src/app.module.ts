63ac13e2816b7116368efc91c44cfb9fd80de8c1
Devin <devdhera@gmail.com>
2019-07-03 21:51:06 +0530

    Base setup for Data Persistence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MaterialRequisitionNotesModule } from './material-requisition-notes/material-requisition-notes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [MaterialRequisitionNotesModule]
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    MaterialRequisitionNotesModule
+  ]
 })
 export class AppModule { }
