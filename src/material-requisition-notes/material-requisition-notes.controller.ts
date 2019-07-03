10fd35b8285c802040450580ed66abe0e9dd56cf
Devin <devdhera@gmail.com>
2019-07-03 13:48:33 +0530

    Configured basic create MRN conroller

--- src/material-requisition-notes/material-requisition-notes.controller.ts
+++ src/material-requisition-notes/material-requisition-notes.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { MaterialRequisitionNotesService } from './material-requisition-notes.service';
-import { MaterialRequisitionNote } from './material-requisition-note.model';
+import { MaterialRequisitionNote, Item } from './material-requisition-note.model';
 
 @Controller('mrns')
 export class MaterialRequisitionNotesController {
@@ -9,5 +9,18 @@ export class MaterialRequisitionNotesController {
     @Get()
     getAllMaterialRequisitionNotes(): MaterialRequisitionNote[] {
         return this.materialRequisitionNotesService.getAllMaterialRequisitionNotes();
+    }
+
+    @Post()
+    createMaterialRequisitionNote(
+        @Body('mrnNo') mrnNo: string,
+        @Body('siteLocation') siteLocation: string,
+        @Body('requestDate') requestDate: Date,
+        @Body('requestedBy') requestedBy: string,
+        @Body('approvedBy') approvedBy: string,
+        @Body('items') items: Item[],
+
+    ): MaterialRequisitionNote {
+        return this.materialRequisitionNotesService.createMaterialRequisitionNote(mrnNo, siteLocation, requestDate, requestedBy, approvedBy, items);
     }
 }
--- src/material-requisition-notes/material-requisition-notes.service.ts
+++ src/material-requisition-notes/material-requisition-notes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { MaterialRequisitionNote } from './material-requisition-note.model';
+import { MaterialRequisitionNote, Item, MaterialRequisitionNoteStatus } from './material-requisition-note.model';
+import * as uuid from 'uuid/v1'
 
 @Injectable()
 export class MaterialRequisitionNotesService {
@@ -7,5 +8,21 @@ export class MaterialRequisitionNotesService {
 
     getAllMaterialRequisitionNotes(): MaterialRequisitionNote[] {
         return this.materialRequisitionNotes;
+    }
+
+    createMaterialRequisitionNote(mrnNo: string, siteLocation: string, requestDate: Date, requestedBy: string, approvedBy: string, items: Item[]): MaterialRequisitionNote {
+        const materialRequisitionNote: MaterialRequisitionNote = {
+            id: uuid(),
+            mrnNo,
+            siteLocation,
+            requestDate,
+            requestedBy,
+            approvedBy,
+            items,
+            status: MaterialRequisitionNoteStatus.OPEN
+        }
+
+        this.materialRequisitionNotes.push(materialRequisitionNote);
+        return materialRequisitionNote;
     }
 }
