f03d34c5fe0747c270e4c1a4a7c2a1a20a7ea46b
Devin <devdhera@gmail.com>
2019-07-04 20:35:40 +0530

    Swagger Configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v2');
+
+  /* Swagger Setup */
+  const options = new DocumentBuilder()
+    .setTitle('HyproDoc - API')
+    .setDescription('REST client documentation for HyproDoc Platform')
+    .setVersion('2.0')
+    .addTag('hyprodoc')
+    .setSchemes('http', 'https')
+    .setBasePath('api/v2')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/v2/docs', app, document);
+  /* End of Swagger Setup */
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
--- src/material-requisition-notes/dto/create-material-requisition-note.dto.ts
+++ src/material-requisition-notes/dto/create-material-requisition-note.dto.ts
@@ -1,22 +1,30 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateMaterialRequisitionNoteDto {
+    @ApiModelProperty()
     @IsNotEmpty()
     mrnNo: string;
 
+    @ApiModelProperty()
     @IsNotEmpty()
     siteLocation: string;
 
+    @ApiModelProperty()
     @IsNotEmpty()
     requestDate: Date;
 
+    @ApiModelProperty()
     @IsNotEmpty()
     requestedBy: string;
 
+    @ApiModelProperty()
     approvedDate: Date;
 
+    @ApiModelProperty()
     approvedBy: string;
 
+    @ApiModelProperty()
     @IsNotEmpty()
     items: string;
 }
