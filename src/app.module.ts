8348a176657db049d8b621cdc44adea5581c16d4
Devin <devdhera@gmail.com>
2019-07-05 11:33:22 +0530

    Base setup for Authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MaterialRequisitionNotesModule } from './material-requisition-notes/material-requisition-notes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    MaterialRequisitionNotesModule
+    MaterialRequisitionNotesModule,
+    AuthModule
   ]
 })
 export class AppModule { }
