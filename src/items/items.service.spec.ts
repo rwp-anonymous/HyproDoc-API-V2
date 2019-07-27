5764861e9a0e1ce85e1d464e293fc86b3a39165d
Devin <devdhera@gmail.com>
2019-07-27 21:49:19 +0530

    Added getItems Test Case

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { ItemsService } from './items.service';
 import { ItemRepository } from './item.repository';
+import { GetItemsFilterDto } from './dto/get-items-filter.dto';
+import { StoreLocations } from './store-location.enum';
+
+const mockUser = { email: 'test@test.com' };
 
 const mockItemRepository = () => ({
-
+    getItems: jest.fn(),
 })
 
 describe('ItemsService', () => {
@@ -20,5 +24,19 @@ describe('ItemsService', () => {
 
         itemsService = await module.get<ItemsService>(ItemsService);
         itemRepository = await module.get<ItemRepository>(ItemRepository);
+    });
+
+    describe('getItems', () => {
+        it('get all items from the repository', async () => {
+            itemRepository.getItems.mockResolvedValue('someValue');
+
+            expect(itemRepository.getItems).not.toHaveBeenCalled();
+
+            const filters: GetItemsFilterDto = { storeLocation: StoreLocations.UGANDA, code: 'some code query' };
+
+            const result = await itemsService.getItems(filters, mockUser);
+            expect(itemRepository.getItems).toHaveBeenCalled();
+            expect(result).toEqual('someValue');
+        });
     });
 });
